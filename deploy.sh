d54a6701ced282e41bd7472973d4eff676999783
Noah Gibbs <noah_gibbs@yahoo.com>
2014-02-04 10:44:18 -0800

    Initial deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,2 @@
+git push
+ssh www@codefol.io "RAILS_ENV=production . ~/.bash_profile && cd checkouts/codefolio_octopress && git pull && rvm use 1.9.3-p125@deployment && bundle && touch tmp/restart.txt"
