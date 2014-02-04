db791254ad3adde45b6c69174f20fed76c7857a8
Noah Gibbs <noah_gibbs@yahoo.com>
2014-02-03 22:24:17 -0800

    Set the Disqus ID for old posts and patch Octopress to use it.

--- convert_posts.rb
+++ convert_posts.rb
@@ -11,10 +11,14 @@ taggings = MultiJson.load File.read "codefolio_taggings_20140201.json"
 puts "Deleting old posts... Don't worry if you see a 'No such file or directory' error!"
 `rm source/_posts/*`
 
+badchar_regexp = /[^a-z1-9A-Z]+/i
+
 posts.each do |post|
   tags = taggings.select { |t| t["post_id"] == post["id"] }
   categories = tags.map { |t| t["tag"] }.select {|t| t != "," }.join ","
   categories = "[#{categories}]" unless categories == ""
+
+  codefolio_url_words = post["title"].gsub badchar_regexp, '-'
 
   post_date = Time.parse post["created_at"]
   post_title = post["title"].tr("'", "").tr('^a-zA-Z0-9', '-').gsub(/-+/, "-").gsub(/^-/, "").gsub(/-$/, "")
@@ -27,6 +31,8 @@ date: #{post_date.strftime("%Y-%m-%d %H:%M")}
 comments: true
 published: #{post["published"] ? "true" : "false"}
 categories: #{categories}
+meta:
+  disqus_id: "http://codefol.io/posts/#{post["id"]}-#{codefolio_url_words}"
 ---
 #{post["body"]}
   CONTENTS
